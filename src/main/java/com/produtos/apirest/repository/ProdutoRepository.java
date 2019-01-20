f77e9ba771b197e9cbbfe344343288be24e6cf62
CHARLES FERREIRA <charlesdccti@gmail.com>
2019-01-20 18:41:44 -0300

    Criando repository

---
+++ src/main/java/com/produtos/apirest/repository/ProdutoRepository.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.produtos.apirest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.produtos.apirest.models.Produto;
+
+/**
+ * @author charles
+ *
+ */
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	
+	Produto findById(long id);
+
+}
