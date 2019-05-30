78108220cb7530bc2e3c68ee1b0c066dd2b4b70b
CHARLES FERREIRA <charlesdccti@gmail.com>
2019-05-30 17:38:32 -0300

    add Segurité JWT

--- src/main/java/com/produtos/apirest/ApirestApplication.java
+++ src/main/java/com/produtos/apirest/ApirestApplication.java
@@ -1,9 +1,11 @@
 package com.produtos.apirest;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ApirestApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/produtos/apirest/repository/UserController.java
@@ -0,0 +1,21 @@
+package com.produtos.apirest.repository;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	@RequestMapping("/home")
+	public String hello () {
+		return "Hello buddy!";
+	}
+
+	@RequestMapping("/users")
+	@ResponseBody
+	public String getUsers() {
+		return "{\"users\":[{\"name\":\"Lucas\", \"country\":\"Brazil\"}," +
+		           "{\"name\":\"Jackie\",\"country\":\"China\"}]}";
+	}
+}
