0f91a7b254344b0123412f13c60a090ccaedf84c
CHARLES FERREIRA <charlesdccti@gmail.com>
2019-05-31 15:05:08 -0300

    add Autorisation des ressources avec Spring Security

--- src/main/java/com/produtos/apirest/resources/UserController.java
+++ src/main/java/com/produtos/apirest/resources/UserController.java
@@ -1,4 +1,4 @@
-package com.produtos.apirest.repository;
+package com.produtos.apirest.resources;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
